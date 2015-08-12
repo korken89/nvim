b729c8315849a9b3fdb6b7965f6d6173404fd519
Emil Fresk <emil.fresk@gmail.com>
2015-08-12 23:55:23 +0200

    Updates to the install script to work from clean installation

--- install.sh
+++ install.sh
@@ -3,14 +3,20 @@
 # Installs everything for neovim and compiles YouCompleteMe
 #
 
+# Get GIT
+sudo pacman -S git --needed --noconfirm
+
+# Get the repo
 cd ~
+rm -f ~/.nvimrc
+mkdir -p ~/.nvim
 git clone https://github.com/korken89/nvim.git ~/.nvim
 ln -s ~/.nvim/nvimrc ~/.nvimrc
 cd ~/.nvim
 git submodule update --init --recursive
 
 # Get CLang
-sudo pacman -S astyle ctags clang --needed
+sudo pacman -S astyle ctags clang --needed --noconfirm
 
 # Compile YouCompleteMe
 cd ~/.nvim/bundle/YouCompleteMe
